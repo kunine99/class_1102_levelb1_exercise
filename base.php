f7230d9e489d117fa0d51b7dfb87eae6470e1ed0
rong <rongyy94job@gmail.com>
2021-12-23 16:01:08 +0800

    完成次選單數顯示

--- base.php
+++ base.php
@@ -130,7 +130,7 @@ class DB{
                     $tmp[]="`$key`='$value'";
                 }
 
-                $sql .=" WHERE ".implode(" AND ".$arg[0])." ".$arg[1];
+                $sql .=" WHERE ".implode(" AND ".$tmp)." ".$arg[1];
 
             break;
             case 1:
@@ -138,9 +138,9 @@ class DB{
                     foreach($arg[0] as $key => $value){
                         $tmp[]="`$key`='$value'";
                     }
-                    $sql .= " WHERE ".implode(" AND ".$arg[0]);
+                    $sql .= " WHERE ".implode(" AND ".$tmp);
                 }else{
-                    $sql .= $arg[1];
+                    $sql .= $arg[0];
                     
                 }
             break;
