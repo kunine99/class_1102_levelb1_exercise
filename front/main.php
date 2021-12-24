614c1175720566982a2daa7847f70ccb33539c70
rong <rongyy94job@gmail.com>
2021-12-24 11:44:32 +0800

    動態文字廣告顯示

--- front/main.php
+++ front/main.php
@@ -1,9 +1,7 @@
 <div class="di"
     style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
 <!-- 這張ad資料表裡 sh1 的才是我要顯示出來的 -->
-    <marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
-    <?php  ?>
-</marquee>
+<?php include "marquee.php";?>
     <div style="height:32px; display:block;"></div>
     <!--正中央-->
     <script>
